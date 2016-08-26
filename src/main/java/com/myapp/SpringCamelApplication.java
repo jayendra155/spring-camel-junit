7189cefc2cb29b989884995f277cf8e342325bf0
jayendra155 <jayendra155@hotmail.com>
2016-08-26 19:10:25 +0530

    .

--- src/main/java/com/myapp/SpringCamelApplication.java
+++ src/main/java/com/myapp/SpringCamelApplication.java
@@ -4,7 +4,6 @@ import org.apache.camel.spring.SpringCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
